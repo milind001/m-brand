3b6186ee76c372d3235401d9b1bcbdd10fc1d81d
milind001 <milindpatil716@gmail.com>
2020-09-09 18:33:46 +0530

    implemented firebase, store authenticated users

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/HomePage';
 import ShopPage from './pages/shop/ShopPage';
 import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up';
 import Header from './components/header/header';
-import { auth } from './firebase/firebase.util';
+import { auth, createUserProfileDocument } from './firebase/firebase.util';
 
 class App extends React.Component {
   constructor(){
@@ -19,10 +19,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-
-      console.log(user);  
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+    
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+         
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      }else{
+        this.setState({currentUser: userAuth});
+      } 
     })
   }
 
